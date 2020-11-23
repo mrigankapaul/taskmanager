2c031369d04eb845164e826f7de205d76d20d59f
mriganka.paul <paul.mriganka@gmail.com>
2020-11-23 17:27:32 -0500

    Added different Routes

--- src/index.js
+++ src/index.js
@@ -1,83 +1,15 @@
-const express = require("express");
-const app = express();
-require("./db/mongoose.js");
-const User = require("./models/user.js");
-const Task = require("./models/task.js");
-const port = process.env.PORT || 3000;
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
-// parses incoming json to object
-app.use(express.json());
+const app = express()
+const port = process.env.PORT || 3000
 
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then((user) => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.status(200).send(users);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
-});
-
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.status(200).send(user);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then((task) => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id
-
-    Task.findById(_id).then((task) => {
-        if (!task) {
-            return res.status(404).send()
-        }
-
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
-  console.log("Server is up on port", port);
-});
+    console.log('Server is up on port ' + port)
+})
