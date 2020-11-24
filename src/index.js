41403cfacea8952a72a8577987e9276e963ef64a
mriganka.paul <paul.mriganka@gmail.com>
2020-11-23 21:06:20 -0500

    After /users/login

--- src/index.js
+++ src/index.js
@@ -14,17 +14,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 
-const myFunction = async () => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+// const myFunction = async () => {
+//     const password = 'Red12345!'
+//     const hashedPassword = await bcrypt.hash(password, 8)
 
-    console.log(password)
-    console.log(hashedPassword)
+//     console.log(password)
+//     console.log(hashedPassword)
 
-    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
-    console.log(isMatch)
-}
+//     const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+//     console.log(isMatch)
+// }
 
-myFunction()
+// myFunction()
