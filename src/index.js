2ceee507d95eed319a24d2872b440a9aca6d7e49
mriganka.paul <paul.mriganka@gmail.com>
2020-11-24 12:35:06 -0500

    Authentication completed

--- src/index.js
+++ src/index.js
@@ -25,15 +25,18 @@ app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
 
-const jwt = require("jsonwebtoken");
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
-    expiresIn: "7 days",
-  });
-  console.log("token", token);
+const Task = require("./models/task");
+const User = require('./models/user')
+const main = async () => {
+  // const task = await Task.findById("5fbd36d7bfabbe1f9453c5e8");
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner);
 
-  const data = jwt.verify(token, "thisismynewcourse");
-  console.log(data);
+  const user = await User.findById("5fbd35cba10b6f1f70aac48d")
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+
+
 };
 
-myFunction();
+main();
